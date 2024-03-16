d7aae14d347d13933eb49497e4196f7ee57b69bd
Michael Berger <m.gonser@icloud.com>
2024-03-16 06:23:07 +0100

    feat(nx-dependency-check): sort outdated packages

--- plugins/nx-dependency-check/src/executors/check-deps/utils/filter-result.ts
+++ plugins/nx-dependency-check/src/executors/check-deps/utils/filter-result.ts
@@ -9,7 +9,21 @@ export function filterResult(params: { result: Array<CompareVersionResult> }): A
    * We only want to return those packages which needs to be updated
    */
 
-  return params.result.filter(
+  const filteredResult = params.result.filter(
     (result) => result.result.major.needsUpdate || result.result.minor.needsUpdate || result.result.patch.needsUpdate
   );
+
+  // sort by major needsupdate then minor then patch and within the groups by package name
+  return filteredResult.sort((a, b) => {
+    if (a.result.major.needsUpdate !== b.result.major.needsUpdate) {
+      return a.result.major.needsUpdate ? -1 : 1;
+    }
+    if (a.result.minor.needsUpdate !== b.result.minor.needsUpdate) {
+      return a.result.minor.needsUpdate ? -1 : 1;
+    }
+    if (a.result.patch.needsUpdate !== b.result.patch.needsUpdate) {
+      return a.result.patch.needsUpdate ? -1 : 1;
+    }
+    return a.packageName.localeCompare(b.packageName);
+  });
 }
